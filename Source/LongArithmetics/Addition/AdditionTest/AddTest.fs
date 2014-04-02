a08f62be52c7e762e5f5ee3bc7990e66a88d4895
AndreyBulgakov <pivolit@gmail.com>
2014-04-02 19:05:21 +0400

    Tests for negative variables
    Redaction gitignore

--- Source/LongArithmetics/Addition/AdditionTest/AddTest.fs
+++ Source/LongArithmetics/Addition/AdditionTest/AddTest.fs
@@ -96,3 +96,96 @@ type Additiontests() =
 
         Assert.AreEqual([|1|], flag)
         Assert.AreEqual(array3, array4)
+
+    [<Test>]
+    member this.ANegBNegSimpleTestNotOverflow() =
+        let array1 = Array.zeroCreate(10000)
+        let array2 = Array.zeroCreate(10000)
+        let array3  = Array.zeroCreate(10000)
+
+        array1.[0] <- -1
+        array1.[9999] <- 1
+
+        array2.[0] <- -1
+        array2.[9999] <- 1
+
+        array3.[0] <- -1
+        array3.[9999] <- 2
+        
+        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
+
+    [<Test>]
+    member this.ANegBNeg1JumpTestNotOverflow() =
+        let array1 = Array.zeroCreate(10000)
+        let array2 = Array.zeroCreate(10000)
+        let array3  = Array.zeroCreate(10000)
+
+        array1.[0] <- -1
+        array1.[9999] <- 9
+
+        array2.[0] <- -1
+        array2.[9999] <- 1
+
+        array3.[0] <- -1
+        array3.[9998] <- 1
+        
+        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
+
+    [<Test>]
+    member this.ANegBNegMore1JumpTestNotOverflow() =
+        let array1 = Array.zeroCreate(10000)
+        let array2 = Array.zeroCreate(10000)
+        let array3  = Array.zeroCreate(10000)
+
+        array1.[0] <- -1
+        for i in 2 .. 9999 do
+            array1.[i] <- 9
+
+        array2.[0] <- -1
+        array2.[9999] <- 1
+
+        array3.[0] <- -1
+        array3.[1] <- 1
+        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
+
+    [<Test>]
+    member this.ANegBNegNotFirstJumpTestNotOverflow() =
+        let array1 = Array.zeroCreate(10000)
+        let array2 = Array.zeroCreate(10000)
+        let array3  = Array.zeroCreate(10000)
+
+        array1.[0] <- -1
+        array1.[9999] <- 1
+        array1.[9998] <- 9
+
+        array2.[0] <- -1
+        array2.[9998] <- 1
+
+        array3.[0] <- -1
+        array3.[9999] <- 1
+        array3.[9997] <- 1
+        
+        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
+
+    [<Test>]
+    member this.ANegBNegMore1JumpTestOverflow() =
+        let array1 = Array.zeroCreate(10000)
+        let array2 = Array.zeroCreate(10000)
+        let array3  = Array.zeroCreate(10000)
+        let flag = [|0|]
+
+        array1.[0] <- -1
+        for i in 1 .. 9999 do
+            array1.[i] <- 9
+
+        array2.[0] <- -1
+        array2.[9999] <- 1
+
+        array3.[0] <- -1
+        let array4 = Addition.Main array1 array2 flag
+        for i in 1 .. 9999 do
+            if array4.[i] = 10
+            then printfn "%A" i
+
+        Assert.AreEqual([|1|], flag)
+        Assert.AreEqual(array3, array4)
