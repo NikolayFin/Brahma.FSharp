e4306562898f3795663b62a967c99ff5a85107af
m13oas <aozertsov@gmail.com>
2014-04-14 14:14:38 +0400

    Redact task

--- Source/LongArithmetics/Addition/Addition/AddBody.fs
+++ Source/LongArithmetics/Addition/Addition/AddBody.fs
@@ -7,7 +7,7 @@ open Brahma.FSharp.OpenCL.Core
 open Microsoft.FSharp.Quotations
 open Brahma.FSharp.OpenCL.Extensions
 
-let Main (array1:array<_>) (array2:array<_>) (flag:array<_>) =
+let Main (array1:array<_>) (operation:array<_>) (array2:array<_>) (flag:array<_>) =
     
     let c = Array.zeroCreate(array1.Length)
 
@@ -25,11 +25,57 @@ let Main (array1:array<_>) (array2:array<_>) (flag:array<_>) =
 
     let command = 
         <@
-            fun (rng:_1D) (a:array<_>) (b:array<_>) (c:array<_>) (fl:array<_>)->
+            fun (rng:_1D) (a:array<_>) (b:array<_>) (c:array<_>) (fl:array<_>) (operation:array<_>)->
                 let r = rng.GlobalID0
                 let counter = ref r
                 let count = ref true
-                if a.[0] = b.[0]
+                
+                if operation.[0] = 1 
+                then 
+                    c.[r] <- a.[r] + b.[r]
+                
+                    while !counter > 0 && !count do
+                        count := false
+                        if !counter > 0 && c.[!counter] > 9 
+                        then
+                            count := true
+                            c.[!counter] <!+ (-1 * 10)
+                            c.[!counter - 1] <! c.[!counter - 1] + 1
+                        counter := !counter - 1
+
+                    if c.[0] > 9
+                    then
+                        c.[0] <!+ (-1 * 10)
+                        fl.[0] <!+ (fl.[0] + 1)
+                if operation.[0] = -1
+                then 
+                    c.[r] <- a.[r] - b.[r]
+
+                    while !counter > 0 && !count do
+                        count := false
+                        if !counter > 0 && c.[!counter] < 0 
+                        then
+                            count := true
+                            c.[!counter] <!+ 10
+                            c.[!counter - 1] <! c.[!counter - 1] - 1
+                        counter := !counter - 1
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+               (* if a.[0] = b.[0]
                 then
                     if r > 0
                     then
@@ -43,6 +89,7 @@ let Main (array1:array<_>) (array2:array<_>) (flag:array<_>) =
                                 c.[!counter] <!+ (-1 * 10)
                                 c.[!counter - 1] <! c.[!counter - 1] + 1
                             counter := !counter - 1
+                       на хосте смотреть
                         
                         if c.[1] > 9
                         then 
@@ -87,28 +134,15 @@ let Main (array1:array<_>) (array2:array<_>) (flag:array<_>) =
                             count := true
                             c.[!counter] <!+ 10
                             c.[!counter - 1] <! (c.[!counter - 1] - 1)
-                    counter := !counter - 1
+                        counter := !counter - 1*)
         @>
 
     let kernel, kernelPrepare, kernelRun = provider.Compile command
     
     let d = (new _1D(array1.Length, localWorkSize))
-
-    let rec digit (arr1:array<_>) (arr2:array<_>) i =
-        if arr1.[i] < arr2.[i]
-            then -1
-            elif  arr1.[i] > arr2.[i]
-                then 1
-                elif i = arr1.Length - 1
-                    then 0
-                    else digit arr1 arr2 (i + 1)
-
-    if digit array1 array2 1 = -1
-        then
-            kernelPrepare d array2 array1 c flag
-        else
-            kernelPrepare d array1 array2 c flag
-
+    
+    kernelPrepare d array1 array2 c flag operation
+    
     let _ = commandQueue.Add(kernelRun()).Finish()
     let _ = commandQueue.Add(c.ToHost provider)
     let _ = commandQueue.Add(flag.ToHost provider)
@@ -117,4 +151,4 @@ let Main (array1:array<_>) (array2:array<_>) (flag:array<_>) =
     provider.Dispose()
     provider.CloseAllBuffers()
     c
-    //ignore (System.Console.Read())
+        //ignore (System.Console.Read())
--- Source/LongArithmetics/Addition/AdditionTest/AddTest.fs
+++ Source/LongArithmetics/Addition/AdditionTest/AddTest.fs
@@ -5,250 +5,139 @@ open Addition
 type Additiontests() =    
     
     [<Test>]
-    member this.APosBPosSimpleTestNotOverflow() =
+    member this.AddiionSimpleTestNotOverflow() =
         let array1 = Array.zeroCreate(10000)
         let array2 = Array.zeroCreate(10000)
         let array3  = Array.zeroCreate(10000)
 
-        array1.[0] <- 1
         array1.[9999] <- 1
 
-        array2.[0] <- 1
         array2.[9999] <- 1
 
-        array3.[0] <- 1
         array3.[9999] <- 2
         
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
+        Assert.AreEqual(array3, (Addition.Main array1 [|1|] array2 [|0|]))
 
     [<Test>]
-    member this.APosBPos1JumpTestNotOverflow() =
+    member this.Addiion1JumpTestNotOverflow() =
         let array1 = Array.zeroCreate(10000)
         let array2 = Array.zeroCreate(10000)
         let array3  = Array.zeroCreate(10000)
 
-        array1.[0] <- 1
         array1.[9999] <- 9
 
-        array2.[0] <- 1
         array2.[9999] <- 1
 
-        array3.[0] <- 1
         array3.[9998] <- 1
         
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
+        Assert.AreEqual(array3, (Addition.Main array1 [|1|] array2 [|0|]))
 
     [<Test>]
-    member this.APosBPosMore1JumpTestNotOverflow() =
+    member this.AddiionMore1JumpTestNotOverflow() =
         let array1 = Array.zeroCreate(10000)
         let array2 = Array.zeroCreate(10000)
         let array3  = Array.zeroCreate(10000)
 
-        array1.[0] <- 1
-        for i in 2 .. 9999 do
+        for i in 1 .. 9999 do
             array1.[i] <- 9
 
-        array2.[0] <- 1
         array2.[9999] <- 1
 
         array3.[0] <- 1
-        array3.[1] <- 1
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
+        Assert.AreEqual(array3, (Addition.Main array1 [|1|] array2 [|0|]))
 
     [<Test>]
-    member this.APosBPosNotFirstJumpTestNotOverflow() =
+    member this.AddiionNotFirstJumpTestNotOverflow() =
         let array1 = Array.zeroCreate(10000)
         let array2 = Array.zeroCreate(10000)
         let array3  = Array.zeroCreate(10000)
 
-        array1.[0] <- 1
         array1.[9999] <- 1
         array1.[9998] <- 9
 
-        array2.[0] <- 1
         array2.[9998] <- 1
 
-        array3.[0] <- 1
         array3.[9999] <- 1
         array3.[9997] <- 1
         
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
+        Assert.AreEqual(array3, (Addition.Main array1 [|1|] array2 [|0|]))
 
     [<Test>]
-    member this.APosBPosMore1JumpTestOverflow() =
+    member this.AddiionMore1JumpTestOverflow() =
         let array1 = Array.zeroCreate(10000)
         let array2 = Array.zeroCreate(10000)
         let array3  = Array.zeroCreate(10000)
         let flag = [|0|]
 
-        array1.[0] <- 1
-        for i in 1 .. 9999 do
+        for i in 0 .. 9999 do
             array1.[i] <- 9
 
-        array2.[0] <- 1
         array2.[9999] <- 1
 
-        array3.[0] <- 1
-        let array4 = Addition.Main array1 array2 flag
+        let array4 = Addition.Main array1 [|1|] array2 flag
+        
+        for i in 1 .. 9999 do
+            array3.[i] <- 0
+
         for i in 1 .. 9999 do
             if array4.[i] = 10
             then printfn "%A" i
 
-        Assert.AreEqual([|1|], flag)
+       // Assert.AreEqual([|1|], flag)
+        Assert.AreEqual(array3, array4)
+
+    
+    [<Test>]
+    member this.SubtractionSimpleTest() =
+        let array1 = Array.zeroCreate(10000)
+        let array2 = Array.zeroCreate(10000)
+        let array3  = Array.zeroCreate(10000)
+
+        array1.[9999] <- 1
+        array1.[9998] <- 9
+
+        array2.[9999] <- 1
+
+        array3.[9998] <- 9
+
+        let array4 = Addition.Main array1 [|-1|] array2 [|0|]
+        
         Assert.AreEqual(array3, array4)
 
     [<Test>]
-    member this.ANegBNegSimpleTestNotOverflow() =
+    member this.Subtraction1Jump() =
         let array1 = Array.zeroCreate(10000)
         let array2 = Array.zeroCreate(10000)
         let array3  = Array.zeroCreate(10000)
 
-        array1.[0] <- -1
-        array1.[9999] <- 1
-
-        array2.[0] <- -1
-        array2.[9999] <- 1
-
-        array3.[0] <- -1
-        array3.[9999] <- 2
-        
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
-
-    [<Test>]
-    member this.ANegBNeg1JumpTestNotOverflow() =
-        let array1 = Array.zeroCreate(10000)
-        let array2 = Array.zeroCreate(10000)
-        let array3  = Array.zeroCreate(10000)
-
-        array1.[0] <- -1
-        array1.[9999] <- 9
-
-        array2.[0] <- -1
-        array2.[9999] <- 1
-
-        array3.[0] <- -1
-        array3.[9998] <- 1
-        
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
-
-    [<Test>]
-    member this.ANegBNegMore1JumpTestNotOverflow() =
-        let array1 = Array.zeroCreate(10000)
-        let array2 = Array.zeroCreate(10000)
-        let array3  = Array.zeroCreate(10000)
-
-        array1.[0] <- -1
-        for i in 2 .. 9999 do
-            array1.[i] <- 9
-
-        array2.[0] <- -1
-        array2.[9999] <- 1
-
-        array3.[0] <- -1
-        array3.[1] <- 1
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
-
-    [<Test>]
-    member this.ANegBNegNotFirstJumpTestNotOverflow() =
-        let array1 = Array.zeroCreate(10000)
-        let array2 = Array.zeroCreate(10000)
-        let array3  = Array.zeroCreate(10000)
-
-        array1.[0] <- -1
         array1.[9999] <- 1
         array1.[9998] <- 9
 
-        array2.[0] <- -1
-        array2.[9998] <- 1
-
-        array3.[0] <- -1
-        array3.[9999] <- 1
-        array3.[9997] <- 1
-        
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
-
-    [<Test>]
-    member this.ANegBNegMore1JumpTestOverflow() =
-        let array1 = Array.zeroCreate(10000)
-        let array2 = Array.zeroCreate(10000)
-        let array3  = Array.zeroCreate(10000)
-        let flag = [|0|]
-
-        array1.[0] <- -1
-        for i in 1 .. 9999 do
-            array1.[i] <- 9
-
-        array2.[0] <- -1
-        array2.[9999] <- 1
-
-        array3.[0] <- -1
-        let array4 = Addition.Main array1 array2 flag
-        for i in 1 .. 9999 do
-            if array4.[i] = 10
-            then printfn "%A" i
-
-        Assert.AreEqual([|1|], flag)
-        Assert.AreEqual(array3, array4)
-
-    [<Test>]
-    member this.APosBNegSimpleTest() =
-        let array1 = Array.zeroCreate(10000)
-        let array2 = Array.zeroCreate(10000)
-        let array3  = Array.zeroCreate(10000)
-
-        array1.[0] <- 1
-        array1.[9999] <- 1
-        array1.[9998] <- 9
-
-        array2.[0] <- -1
-        array2.[9998] <- 1
-
-        array3.[0] <- 1
-        array3.[9999] <- 1
-        array3.[9998] <- 8
-        
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
-
-    [<Test>]
-    member this.APosBNeg1Jump() =
-        let array1 = Array.zeroCreate(10000)
-        let array2 = Array.zeroCreate(10000)
-        let array3  = Array.zeroCreate(10000)
-
-        array1.[0] <- 1
-        array1.[9999] <- 1
-        array1.[9998] <- 9
-
-        array2.[0] <- -1
         array2.[9999] <- 9
 
-        array3.[0] <- 1
         array3.[9999] <- 2
         array3.[9998] <- 8
         
-        Assert.AreEqual(array3, (Addition.Main array1 array2 [|0|]))
+        Assert.AreEqual(array3, (Addition.Main array1 [|-1|] array2 [|0|]))
 
     [<Test>]
-    member this.APosBNegMore1Jump() =
+    member this.SubtractionMore1Jump() =
         let array1 = Array.zeroCreate(10000)
         let array2 = Array.zeroCreate(10000)
         let array3  = Array.zeroCreate(10000)
 
-        array1.[0] <- 1
-        array1.[1] <- 9
-        for i in 2 .. 9999 do
+        array1.[0] <- 9
+        for i in 1 .. 9999 do
             array1.[i] <- 1
 
-        array2.[0] <- -1
-        for i in 2 .. 9999 do
+        for i in 1 .. 9999 do
             array2.[i] <- 9
 
-        array3.[0] <- 1
         array3.[9999] <- 2
-        for i in 2 .. 9998 do
+        for i in 1 .. 9998 do
             array3.[i] <- 1
-        array3.[1] <- 8
-        let array4 = (Addition.Main array1 array2 [|0|])
+        array3.[0] <- 8
+        let array4 = (Addition.Main array1 [|-1|] array2 [|0|])
         for i in 0 .. 9999 do
             if array4.[i] = 2
             then printfn "%A" i
